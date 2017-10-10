5d8149f83bf8b697f5dc24b299d885387b5e07f4
AlexsaniKY <cerafem@gmail.com>
2017-10-10 11:11:49 -0400

    added commenting and corrected a compiler warning

--- main.c
+++ main.c
@@ -20,10 +20,12 @@ class Command{
 		pid_t	pid;
 		pid = fork();
 		if (pid < 0){	//error
-			exit(EXIT_FAILURE);
+			return;
 		} 
 		else if (pid == 0){	/* Child Process */
+			//expand packed parameters to execlp
 			execlp(file, args..., NULL);
+			//failure state if execlp returns
 			exit(EXIT_FAILURE);
 		}
 		else{  		/* parent process */
@@ -31,26 +33,32 @@ class Command{
 		}
 	}
 
-	//executes an execvp statement with 
+	//executes an execvp statement with a vector of program arguments
 	static void fork_execvp(string file, vector<string> argv){
-		char** ptr;
-		const char* arg_arr[argv.size() + 1];
+		//pointer for vector to char** conversion
+		char** ptr; 
+		// char pointer array for execvp arguments
+		const char* arg_arr[argv.size() + 1]; 
+		//point all char* to cstring representations of strings in vector
 		for (unsigned int i = 0; i < argv.size(); i++){
 			arg_arr[i] = argv[i].c_str();
 		}
+		//null terminated argument list
 		arg_arr[argv.size()] = NULL;
 	
 		pid_t	pid;
 		pid = fork();
 		if (pid < 0){	//error
-			exit(EXIT_FAILURE);
+			return;
 		} 
 		else if (pid == 0){	/* Child Process */
 			ptr = (char**)arg_arr;
 			execvp(file.c_str(), ptr);
+			//failure state if execvp returns
 			exit(EXIT_FAILURE);
 		}
 		else{  		/* parent process */
+			//wait for status change of child
 			waitpid(pid, NULL, 0);
 		}
 	}
@@ -64,16 +72,17 @@ class Command{
 
 	//Changes the directory to the supplied location.
 	//If no location is supplied it simply prints the current one
-	static int cd(string directory){
+	static void cd(string directory){
 		if(directory == ""){
 			cout << get_path() << endl;
-			return 1;
+			return;
 		}
 		int status = chdir(directory.c_str());
+		if (status == -1){
+			cout << "error in opening directory \"" << directory << "\"" << endl; 			
+			return;		
+		}
 		fork_execlp("/bin/ls","ls");
-		if (status<0)
-			return 0;
-		return 1;
 	}
 
 	//clears screen
@@ -112,6 +121,7 @@ class Command{
 		string filename, program;
 
 		stream >> filename;
+		//if there is not a '/' in the path, it is interpreted as a program and needs an empty second argument.
 		if(filename.find("/") == string::npos){
 			vec.push_back("");
 		}
@@ -121,11 +131,11 @@ class Command{
 };
 
 
-
+//decision making function to take input and apply known commands, 
+//or fall through to execute with exec statement
 void cmd(string input){
 	vector<string> split = split_string(input);
 
-	int status = -1;
 	string selection;
 	string options;
 
@@ -137,7 +147,7 @@ void cmd(string input){
 
 	//change directory
 	if (selection == "cd"){
-		status = Command::cd(options);
+		Command::cd(options);
 	}
 	//clear screen
 	else if (selection == "clr"){
@@ -151,22 +161,22 @@ void cmd(string input){
 	else if(selection == "echo"){
 		Command::echo(split, 1);
 	}
+	//exit program
 	else if(selection == "quit"){
 		exit(0);	
 	}
 	else{
 		if(VERBOSE) cout << "attempting to run program: " << input << endl;
-		Command::run(input);
-		//cout << "unknown request" << endl;	
+		Command::run(input);	
 	}
 }
 
+//fork process to use cmd function in a child process
 void forked_cmd(string input){
 	pid_t	pid;
 	pid = fork();
 	if (pid < 0){	//error
 		cout << "An error occurred in creating child process" << endl;
-		exit(0);
 	} 
 	else if (pid == 0){	/* Child Process */
 		cmd(input);
@@ -180,7 +190,7 @@ void forked_cmd(string input){
 
 }
 
-
+//Entry point
 int main(int argc, char *argv[]){
 	if(argc > 1)
 		if(strcmp(argv[1], "-v") == 0)
@@ -190,24 +200,26 @@ int main(int argc, char *argv[]){
 	string whitespace = " \t\f\v\n\r";
 	char last_char = ' ';
 	size_t last_char_index;
+
 	while(true){
 		//prompt
 		cout << Command::get_path() << ": ";
 		//get input
 		getline(cin, input);
+		//keep last non-whitespace character for comparison
 		last_char_index = input.find_last_not_of(whitespace);
 		if(last_char_index != string::npos)
 			last_char = input[last_char_index];
 		else last_char = ' ';
-
+		//ampersand is a fork instruction for the execution
 		if(last_char == '&'){
 			if(VERBOSE) cout << "forking a command" << endl;
 			forked_cmd(input.substr(0, last_char_index));
 		}
+		//otherwise execute the string inline
 		else cmd(input);
-		
+		//check for zombie processes waiting for removal
 		waitpid(-1, NULL, WNOHANG);
-
 	}
 	return 0;
 }
